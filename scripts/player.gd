01bf6f550c07a7ab217e5be98e1597b32cefa367
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-13 20:28:15 +0000

    Updated player logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,37 +1,35 @@
 extends Area2D
 
-const GRAVITY := Vector2(0, 6.674)
+const GRAVITY := Vector2(0, 6.675)
 
 var velocity: Vector2 = Vector2(0, 0)
 var max_angle: float = deg_to_rad(35.0)
 var angle_move: float = deg_to_rad(80.0)
 var fast_move: float = deg_to_rad(80.0)
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass
+	pass
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  if Input.is_key_pressed(KEY_SPACE):
-    $AnimatedSprite2D.play("flap")
-    self.velocity = Vector2(0, -5)
-  else:
-    self.velocity += GRAVITY * delta
+	if Input.is_key_pressed(KEY_SPACE):
+		$AnimatedSprite2D.play("flap")
+		self.velocity = Vector2(0, -5)
+	else:
+		self.velocity += GRAVITY * delta
 
-  var dir = 1 if self.velocity.y > 0 else -1
+	self.velocity.y = clamp(self.velocity.y, -7, 7)
 
-  if self.rotation > 0 and dir < 0:  
-    self.rotation = clampf(
-      self.rotation + dir * fast_move * delta, 
-      -max_angle, max_angle)
-  elif self.rotation < 0 and dir > 0:
-    self.rotation = clampf(
-      self.rotation + dir * fast_move * delta, 
-      -max_angle, max_angle)
-  else:
-    self.rotation = clampf(
-      self.rotation + dir * angle_move * delta, 
-      -max_angle, max_angle)
+	self.position += self.velocity
 
-  self.position += self.velocity
+	var dir = 1 if self.velocity.y > 0 else -1
+
+	if self.rotation > 0 and dir < 0:
+		self.rotation = clampf(self.rotation + dir * fast_move * delta, -max_angle, max_angle)
+	elif self.rotation < 0 and dir > 0:
+		self.rotation = clampf(self.rotation + dir * fast_move * delta, -max_angle, max_angle)
+	else:
+		self.rotation = clampf(self.rotation + dir * angle_move * delta, -max_angle, max_angle)
